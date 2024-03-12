6e14d3b4d2d7f5affcd832fc268eed44e0c79db7
H. Walid <h.walid@hexaee.com>
2024-03-12 17:58:45 +0100

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_Commerce_MVC_.NET8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
